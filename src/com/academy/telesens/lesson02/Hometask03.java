75f448629ed7e55af95f27dac0e93a474ea3435a
olegmalin1 <olegmalin1@gmail>
2020-12-01 01:33:19 +0200

    first task

--- src/com/academy/telesens/lesson02/Hometask03.java
+++ src/com/academy/telesens/lesson02/Hometask03.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class Hometask03 {
     public static void main(String[] args) {
 
-        int divisionResult, remainder;
+        int divisionResult, remainder, numberOne, numberTwo;
 
         //Ask to enter numbers via console
 
         if (args.length == 0) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter first natural number");
-        int numberOne = scanner.nextInt();
+        numberOne = scanner.nextInt();
         System.out.println("Enter second natural number");
-        int numberTwo = scanner.nextInt();
+        numberTwo = scanner.nextInt();
         divisionResult = numberOne/numberTwo;
         remainder = numberOne%numberTwo;
 
@@ -27,13 +27,13 @@ public class Hometask03 {
             System.out.println("Remainder is " + remainder);
         }
         } else {        //Get numbers from arguments
-            int a = Integer.parseInt(args[0]);
-            int b = Integer.parseInt(args[1]);
-            divisionResult = a/b;
-            remainder = a%b;
+            numberOne = Integer.parseInt(args[0]);
+            numberTwo = Integer.parseInt(args[1]);
+            divisionResult = numberOne/numberTwo;
+            remainder = numberOne%numberTwo;
 
-            System.out.println("First number is " + a);
-            System.out.println("Second number is " + b);
+            System.out.println("First number is " + numberOne);
+            System.out.println("Second number is " + numberTwo);
             System.out.println("First number / second number = " + divisionResult);
             System.out.println("Remainder is " + remainder);
 
